85c01d75bcad51931f4188262af57bcd3ed62a84
Jim Strother <strotherwebdev@gmail.com>
2016-01-10 01:55:01 +0000

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,15 @@ $(document).ready(function() {
 });
 
 function checkFizzBuzz(num) {
-    if (num % 1 != 0) {
-        for (var i = 1; i <= num; i++) {
-            if ((i % 3 == 0) && (i % 5 == 0)) {
-                $('#fizzbuzz').append('FizzBuzz<br />');
-            } else if (i % 3 == 0) {
-                $('#fizzbuzz').append('Fizz<br />');
-            } else if (i % 5 == 0) {
-                $('#fizzbuzz').append('Buzz<br />');
-            } else {
-                $('#fizzbuzz').append(i + '<br />');
-            }
+    for (var i = 1; i <= num; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            $('#fizzbuzz').append('FizzBuzz<br />');
+        } else if (i % 3 == 0) {
+            $('#fizzbuzz').append('Fizz<br />');
+        } else if (i % 5 == 0) {
+            $('#fizzbuzz').append('Buzz<br />');
+        } else {
+            $('#fizzbuzz').append(i + '<br />');
         }
-    } else {
-        $('#fizzbuzz').append('Sorry, but I just can\'t work with decimals.  I never got that far in school.');
     }
 }
